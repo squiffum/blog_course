6e506e5252e518ffdac40f98d5cac5768c62f559
squiffum <squiffum@gmail.com>
2016-07-12 16:23:38 +0200

    Used formspring for contact page, instead of whatever

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :projects
-  resources :contacts, only: [:new, :create]
+  resources :contacts
   get "welcome/index"
   root "welcome#index"
 end
